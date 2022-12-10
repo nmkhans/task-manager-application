5f1b4e13527671432b42ef5a7858404f5ec4e638
Moin Khan <nurmoin05@gmail.com>
2022-12-10 20:49:05 +0600

    home page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>My Task manager app</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
