ec85dd63b85763b40b940b1a1fe07216947189d3
Moin Khan <nurmoin05@gmail.com>
2022-12-10 21:00:55 +0600

    readux setup for state management

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
