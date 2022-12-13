9f8c65bb98ca41ebfce47c1ab360e5cce5593637
Moin Khan <nurmoin05@gmail.com>
2022-12-13 23:41:09 +0600

    loading top bar added

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Loading.css";
+
+const Loading = () => {
+    return (
+        <div className="Loading">
+            <div className="loading-bar"></div>
+        </div>
+    );
+};
+
+export default Loading;
