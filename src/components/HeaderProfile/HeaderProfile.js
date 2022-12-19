ac0e5c722af1664d2c23a881a858a9e322b73d70
Moin Khan <nurmoin05@gmail.com>
2022-12-19 21:41:27 +0600

    Header Profile setion updated

--- src/components/HeaderProfile/HeaderProfile.js
+++ src/components/HeaderProfile/HeaderProfile.js
@@ -6,7 +6,7 @@ const HeaderProfile = () => {
             <li>
                 <div className="relative">
                     <div className="avatar">
-                        <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                        <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                             <img src="https://placeimg.com/192/192/people" alt="profile" />
                         </div>
                     </div>
--- src/components/ProfileDetail/ProfileDetail.js
+++ src/components/ProfileDetail/ProfileDetail.js
@@ -1,36 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from "framer-motion";
-import { FiLogIn } from "react-icons/fi";
+import { AiOutlineUser } from "react-icons/ai";
+import { FiLogOut } from "react-icons/fi";
 
 const ProfileDetail = () => {
     return (
         <motion.div
             initial={{ scale: 0 }}
             whileInView={{ scale: 1 }}
-            style={{originY: 0}}
-            transition={{duration: .3}}
+            style={{ originY: 0 }}
+            transition={{ duration: .3 }}
             className="bg-base-100 w-[300px] rounded-lg shadow-lg py-5"
         >
-            <div className="pb-3">
-                <h3 className="text-lg font-semibold text-center">Hello Moin Khan</h3>
+            <div className="pb-3 border-b">
+                <div className="text-center">
+                    <div className="avatar">
+                        <div className="w-14 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                            <img src="https://placeimg.com/192/192/people" alt="profile" />
+                        </div>
+                    </div>
+                </div>
+                <h3 className="text-lg text-slate-800 font-semibold text-center">
+                    Moin Khan
+                </h3>
             </div>
             <li className="block">
                 <div className="block hover:bg-primary hover:text-base-100 hover:pl-10 transition-all text-center">
-                    <Link className="flex items-center justify-center" to="/login">
-                        <span>Login</span>
-                        <span className="ml-1"><FiLogIn /></span>
+                    <Link className="flex items-center justify-center" to="/profile">
+                        <span className="mr-1"><AiOutlineUser /></span>
+                        <span>Profile</span>
                     </Link>
                 </div>
             </li>
             <li className="block">
-                <div className="block hover:bg-primary hover:text-base-100 text-center">
-                    <Link to="/register">Register</Link>
-                </div>
-            </li>
-            <li className="block">
-                <div className="block hover:bg-primary hover:text-base-100 text-center">
-                    Logout
+                <div className="block hover:bg-primary hover:text-base-100 hover:pl-10 transition-all text-center">
+                    <Link className="flex items-center justify-center" to="/profile">
+                        <span className="mr-1"><FiLogOut /></span>
+                        <span>Logout</span>
+                    </Link>
                 </div>
             </li>
         </motion.div>
