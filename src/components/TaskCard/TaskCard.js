e13cdabed50fb7cb5e3b107ae6d7543da8dfb1b4
Moin Khan <nurmoin05@gmail.com>
2022-12-22 22:50:10 +0600

    task card added

---
+++ src/components/TaskCard/TaskCard.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { motion } from "framer-motion";
+import { BsCalendarCheck } from "react-icons/bs";
+import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
+
+const TaskCard = ({ detail }) => {
+    console.log(detail)
+    return (
+        <motion.div
+            className="card bg-base-100 shadow-xl"
+            initial={{ opacity: 0, scaleY: 0, translateY: 200 }}
+            animate={{ opacity: 1, scaleY: 1, translateY: 0 }}
+            transition={{ duration: .5 }}
+        >
+            <div className="card-body">
+                <h2 className="text-lg text-slate-700 font-semibold">{detail.title}</h2>
+                <p className="text-md text-slate-700">
+                    {detail.description}
+                </p>
+                <div className="flex flex-row justify-between">
+                    <div>
+                        <div className="inline-flex items-center text-slate-700">
+                            <span className="mr-2"><BsCalendarCheck /></span>
+                            <span>{detail.createdDate}</span>
+                        </div>
+                        <button className="text-primary mx-2"><AiOutlineEdit /></button>
+                        <button className="text-primary"><AiOutlineDelete /></button>
+                    </div>
+                    <div>
+                        <div className="badge badge-primary text-white">{detail.status}</div>
+                    </div>
+                </div>
+            </div>
+        </motion.div>
+    );
+};
+
+export default TaskCard;
--- src/pages/NewTask/NewTask.js
+++ src/pages/NewTask/NewTask.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from "react-redux";
 import { useGetTasksQuery } from '../../redux/api/apiSlice';
 import Loading from './../../components/Loading/Loading';
+import TaskCard from './../../components/TaskCard/TaskCard';
 
 const NewTask = () => {
     const user = useSelector(state => state.user.user);
@@ -13,14 +14,13 @@ const NewTask = () => {
 
     if(isLoading) return <Loading />
 
-
     return (
         <div className="py-5 px-10">
             <div className="mb-5">
                 <h2 className="text-2xl text-slate-700 font-semibold">New Task</h2>
             </div>
-            <div>
-                
+            <div className="grid grid-cols-3 gap-[50px]">
+                {data.data.map(detail => <TaskCard key={detail._id} detail={detail} />)}
             </div>
         </div>
     );
