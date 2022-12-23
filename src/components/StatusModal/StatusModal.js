f47e30bec6e49356dcae09c53599847087652e76
Moin Khan <nurmoin05@gmail.com>
2022-12-23 14:12:14 +0600

    select option added in modal

--- src/components/StatusModal/StatusModal.js
+++ src/components/StatusModal/StatusModal.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 const StatusModal = ({ taskInfo }) => {
-    console.log(taskInfo)
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        const status = data.status;
+        console.log(status)
+    }
 
     return (
         <div>
             <input type="checkbox" id="status-modal" className="modal-toggle" />
             <label htmlFor="status-modal" className="modal cursor-pointer">
-                <label className="modal-box relative" htmlFor="">
+                <label className="modal-box relative" htmlFor="#">
                     <h3 className="text-xl text-slate-700 text-center font-bold">Update Status</h3>
-                    <div className="text-center">
-                        <p className="py-4"></p>
+                    <div className="text-center mt-5">
+                        <form onChange={handleSubmit(onSubmit)}>
+                            <select
+                                className="select select-bordered w-full"
+                                {...register("status")}
+                            >
+                                <option value="new">new</option>
+                                <option value="pending">pending</option>
+                                <option value="completed">completed</option>
+                                <option value="canceled">canceled</option>
+                            </select>
+                        </form>
                     </div>
                 </label>
             </label>
