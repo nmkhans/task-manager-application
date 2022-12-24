07ce858b854dfa577511957730d79afb814b3aa6
Moin Khan <nurmoin05@gmail.com>
2022-12-24 17:35:49 +0600

    responside issue fixed

--- src/components/NotAvailable/NotAvailable.js
+++ src/components/NotAvailable/NotAvailable.js
@@ -4,12 +4,12 @@ import image from "../../assets/not_available.svg";
 const NotAvailable = () => {
     return (
         <div className="flex flex-col justify-center items-center">
-            <div className="w-[400px]">
+            <div className="w-[300px] md:w-[400px] lg:w-[400px]">
                 <img src={image} alt="Task Not Available" />
             </div>
-            <div>
+            <div className="flex flex-col items-center justify-center">
                 <h2 className="text-2xl text-slate-700 font-bold">No Task Available!</h2>
-                <p className="text-lg text-slate-700 font-normal -ml-10">Create your tasks and show on lists...</p>
+                <p className="text-lg text-slate-700 font-normal ">Create your tasks and show on lists...</p>
             </div>
         </div>
     );
